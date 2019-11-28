e6a4564d7ef8dc9e645024f19459a1eabe2b2496
junblee <topguide82@gmail.com>
2019-11-28 23:26:41 +0900

    one component jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Func from './test';
 
 function App() {
   return (
     <div className="App">
       testing server
+      <Func/>
     </div>
   );
 }
---
+++ src/test.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Func() {
+    return <h3> I Love you </h3>
+}
+
+export default Func;
