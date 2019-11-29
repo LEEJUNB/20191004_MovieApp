f863058fd848a9b8d9d6cda6b4b378738d4d0170
junblee <topguide82@gmail.com>
2019-11-29 23:45:47 +0900

    components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import Func from './test';
+
+function Func(props) {
+  console.log(props);
+  return <h1>Good</h1>
+}
 
 function App() {
   return (
     <div className="App">
       testing server
-      <Func/>
+      <Func 
+        nameman="num1"
+        something={true}
+        arr={['hood','robin',1,2,true]}
+      />
     </div>
   );
 }
