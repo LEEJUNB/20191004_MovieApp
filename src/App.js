4e36d5fd3845006b13bd2d77c7112e05627b5c0b
junblee <topguide82@gmail.com>
2019-12-02 23:12:10 +0900

    JSX props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-function Func(props) {
-return <h1> Good {props}</h1>
+// function Func(props) { 
+//   console.log(props.fav); // fav는 props object안에 있다.
+// return <h1> Good {props.fav}</h1>
+// }
+
+// jsx = HTML + JS
+
+function Func({fav}) { // Components는 대문자 이기에 Func. fav는 Func의 argument. {fav}는 props의 내부에서 fav를 가져오는 것을 의미
+  console.log(fav);
+return <h1> Good {fav}</h1>
 }
 
 
@@ -9,11 +17,7 @@ function App() {
   return (
     <div className="App">
       testing server
-      <Func props="L"/>
-      <Func props="O"/>
-      <Func props="V"/>
-      <Func props="E"/>
-      />
+      <Func fav="L"/>
     </div>
   );
 }
